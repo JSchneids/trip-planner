e6b830fe12c0847692a32aa3c48fdfa33fe9314e
Jakob Schneider <jakobschneider28@yahoo.com>
2018-07-31 22:55:07 -0500

    Initial commit

---
+++ src/main/java/launchcode/com/tripplanner/TripPlannerApplication.java
@@ -0,0 +1,12 @@
+package launchcode.com.tripplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripPlannerApplication.class, args);
+	}
+}
---
+++ src/test/java/launchcode/com/tripplanner/TripPlannerApplicationTests.java
@@ -0,0 +1,16 @@
+package launchcode.com.tripplanner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TripPlannerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

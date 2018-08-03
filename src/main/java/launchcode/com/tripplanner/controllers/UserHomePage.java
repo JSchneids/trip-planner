7bb5b183c6743bd26e04a5933e526da46a12ee52
Jakob Schneider <jakobschneider28@yahoo.com>
2018-08-03 17:30:35 -0500

    Set up templates

---
+++ src/main/java/launchcode/com/tripplanner/controllers/UserHomePage.java
@@ -0,0 +1,16 @@
+package launchcode.com.tripplanner.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class UserHomePage {
+
+    @RequestMapping(value = "userhomepage")
+    public String UserHomePage() {
+            return "UserHomePage";
+    }
+}
+// TODO Make welcome page say user's currently planned trips
--- src/main/java/launchcode/com/tripplanner/controllers/WelcomeController.java
+++ src/main/java/launchcode/com/tripplanner/controllers/WelcomeController.java
@@ -3,15 +3,13 @@ package launchcode.com.tripplanner.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 
 public class WelcomeController {
 
         @RequestMapping(value = "")
-        @ResponseBody
-        public String index() {
-            return "Welcome to Trip Planner";
+        public String Welcome() {
+            return "Welcome";
         }
 }
